754c5b54053719ab082c26375a4488130570347f
devitoangelo <angelodevito4@gmail.com>
2024-02-20 16:38:02 +0100

    console.log- input-button

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+
+let fullName = document.getElementById("age")
+console.log(fullName.value);
+document.querySelector("button").addEventListener("click", function () {
+
+    console.log(age.value);
+})
+
+
+let km = document.getElementById("km")
+console.log(km.value)
+document.querySelector("button").addEventListener("click", function () {
+    console.log(km.value);
+})
+
+
+
+
+
